0e535205ac2092e8d6d1ef1a1262c7332bda3c04
a2c <a2c.ambivalent@outlook.jp>
2021-06-13 16:36:42 +0900

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Header } from './Header';
+import { Top } from './Top';
+import { DailyTop } from './daily/pages/DailyTop';
+import { CategoryView } from './daily/pages/CategoryView';
+import {DailyDetail} from './daily/pages/DailyDetail';
+import { Profile} from './profile/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export const App = () => {  
+  return(
+    <div>
+      <Router>
+        <Header />
+        <div>
+          <Switch>
+              <Route exact path='/' component={Top} />
+              <Route exact path='/daily' component={DailyTop} />
+              <Route exact path='/daily/:id' component={DailyDetail} />
+              <Route exact path='/daily/category/:cat' component={CategoryView}/>
+              <Route path='/profile' component={Profile}/>
+              <Route render={() => <h4>not found...</h4>} />
+          </Switch>
+        </div>
+      </Router>
     </div>
-  );
+  )  
 }
-
-export default App;
---
+++ frontend/src/daily/components/DailyContent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import img1 from '../../images/daily/image1.jpg';
+import img2 from '../../images/daily/image2.jpg';
+import img3 from '../../images/daily/image3.jpg';
+import img4 from '../../images/daily/image4.jpg';
+
+export const DailyContent = (daily) => {
+    let eva;
+    if(daily.evaluation === 'perfect'){
+        eva = img1;
+    }else if(daily.evaluation === 'good'){
+        eva = img2;
+    }else if(daily.evaluation === 'soso'){
+        eva = img3;
+    }else{
+        eva = img4;
+    }
+
+    return(
+        <div>
+            <Link to={`daily/${daily.id}`}> <h1>{daily.date}</h1> </Link> 
+            <img src={eva}/>
+        </div>
+    )
+}
